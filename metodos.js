12c7ea60c394573b3c99d677d0959c03c10cda56
ElvisSarchi <sacianex2@gmail.com>
2023-03-27 14:03:06 -0500

    fix

--- metodos.js
+++ metodos.js
@@ -5,59 +5,67 @@ require("dotenv").config();
 var url = process.env.WSSRI;
 function parserXMLtoString(xml) {
   //return promise
-  return new Promise((resolve, reject) => {
-    parseString(xml, function (err, result) {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(result);
-      }
+  try {
+    return new Promise((resolve, reject) => {
+      parseString(xml, function (err, result) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      });
     });
-  });
+  } catch (err) {
+    console.log(err);
+  }
 }
 export async function searchXML(claveAccesoComprobante) {
-  return new Promise((resolve, reject) => {
-    var args = {
-      claveAccesoComprobante: claveAccesoComprobante,
-    };
-    soap.createClient(url, function (err, client) {
-      if (err) {
-        reject(err);
-      } else {
-        client.autorizacionComprobante(args, async function (err, result) {
-          if (err) {
-            reject(err);
-          } else {
-            const estado =
-              result.RespuestaAutorizacionComprobante.autorizaciones
-                .autorizacion.estado;
-            const numeroAutorizacion =
-              result.RespuestaAutorizacionComprobante.autorizaciones
-                .autorizacion.numeroAutorizacion;
-            const fechaAutorizacion =
-              result.RespuestaAutorizacionComprobante.autorizaciones
-                .autorizacion.fechaAutorizacion;
-            const ambiente =
-              result.RespuestaAutorizacionComprobante.autorizaciones
-                .autorizacion.ambiente;
-            const comprobante =
-              result.RespuestaAutorizacionComprobante.autorizaciones
-                .autorizacion.comprobante;
-            const mensajes =
-              result.RespuestaAutorizacionComprobante.autorizaciones
-                .autorizacion.mensajes;
-            const comprobanteJson = await parserXMLtoString(comprobante);
-            resolve({
-              estado,
-              numeroAutorizacion,
-              fechaAutorizacion,
-              ambiente,
-              comprobante: comprobanteJson,
-              mensajes,
-            });
-          }
-        });
-      }
+  try {
+    return new Promise((resolve, reject) => {
+      var args = {
+        claveAccesoComprobante: claveAccesoComprobante,
+      };
+      soap.createClient(url, function (err, client) {
+        if (err) {
+          reject(err);
+        } else {
+          client.autorizacionComprobante(args, async function (err, result) {
+            if (err) {
+              reject(err);
+            } else {
+              const estado =
+                result.RespuestaAutorizacionComprobante.autorizaciones
+                  .autorizacion.estado;
+              const numeroAutorizacion =
+                result.RespuestaAutorizacionComprobante.autorizaciones
+                  .autorizacion.numeroAutorizacion;
+              const fechaAutorizacion =
+                result.RespuestaAutorizacionComprobante.autorizaciones
+                  .autorizacion.fechaAutorizacion;
+              const ambiente =
+                result.RespuestaAutorizacionComprobante.autorizaciones
+                  .autorizacion.ambiente;
+              const comprobante =
+                result.RespuestaAutorizacionComprobante.autorizaciones
+                  .autorizacion.comprobante;
+              const mensajes =
+                result.RespuestaAutorizacionComprobante.autorizaciones
+                  .autorizacion.mensajes;
+              const comprobanteJson = await parserXMLtoString(comprobante);
+              resolve({
+                estado,
+                numeroAutorizacion,
+                fechaAutorizacion,
+                ambiente,
+                comprobante: comprobanteJson,
+                mensajes,
+              });
+            }
+          });
+        }
+      });
     });
-  });
+  } catch (err) {
+    console.log(err);
+  }
 }
