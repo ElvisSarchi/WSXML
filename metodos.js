1ec4039b3dba2ca32895700754bb85b99222e30d
ElvisSarchi <sacianex2@gmail.com>
2023-03-27 14:16:15 -0500

    fix

--- metodos.js
+++ metodos.js
@@ -6,6 +6,7 @@ var url = process.env.WSSRI;
 function parserXMLtoString(xml) {
   //return promise
   try {
+    if(xml)
     return new Promise((resolve, reject) => {
       parseString(xml, function (err, result) {
         if (err) {
